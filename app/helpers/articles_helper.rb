9b45efc3c7ff91a1937847df799ac01e988ffdc1
hari <vharissh14@gmail.com>
2014-10-24 13:06:02 +0530

    Heroku Deploy

---
+++ app/helpers/articles_helper.rb
@@ -0,0 +1,9 @@
+module ArticlesHelper
+	def article_params
+	  params.require(:article).permit(:title, :body)
+	end
+	  def article_params
+	    params.require(:article).permit(:title, :body, :tag_list)
+	  end
+
+end
