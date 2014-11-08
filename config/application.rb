b49b1a947911dc2b318731cd772c93840285f6f7
hari <vharissh14@gmail.com>
2014-11-08 22:33:15 +0530

    config removed

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require File.expand_path('../boot', __FILE__)
-config.action_mailer.default_url_options = { host: "localhost:3000" }
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
