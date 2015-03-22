87421fe7906f52eea2c325b58e22dbf0ea4a20f7
Sree Harissh V <vharissh14@gmail.com>
2015-03-22 14:15:59 +0530

    Update setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "gmail.com",
   :user_name            => "vharissh14",
-  :password             => "brvhari@3",
+  :password             => "dragons4",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
